d9d3e4a74c2c742bdf4d8ede649a6ea3974538fb
FelixAnders555 <167901183+FelixAnders555@users.noreply.github.com>
2024-05-08 08:57:49 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,14 +3,14 @@ basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P0, 1)
     basic.pause(500)
     basic.setLedColor(0xff0000)
-    Messwert = pins.digitalReadPin(DigitalPin.P1)
-    if (Messwert > 22) {
+    Messwert = pins.analogReadPin(AnalogPin.P1)
+    if (Messwert > 100) {
         basic.setLedColor(0x00ff00)
         motors.motorPower(-100)
         basic.pause(3000)
     } else {
         if (input.pinIsPressed(TouchPin.P2)) {
-            basic.pause(5000)
+            basic.pause(2000)
             motors.motorPower(100)
             basic.setLedColor(0xff0000)
         } else {
